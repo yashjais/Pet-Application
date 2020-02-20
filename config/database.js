b3fe6d3652a5cfa859d678d8c4cde42d1b8ce804
Yash Jaiswal <jais.yash01@gmail.com>
2020-02-20 11:18:59 +0530

    initial commit - backend setup

---
+++ config/database.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+const setUPDB = () => {
+    mongoose.connect('mongodb://localhost:27017/notes-redux',{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify : false, useCreateIndex: true })
+        .then(res => console.log('connected to db'))
+        .catch(err => console.log(err))
+}
+
+module.exports = setUPDB
---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const setUpDB = require('./config/database')
+const router = require('./config/routes')
+
+const port = 3020
+const app = express()
+setUpDB()
+
+app.use('/', router)
+
+app.listen(port, () => {
+    console.log('listening on the port', port)
+})
+
