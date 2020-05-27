7258e3f47753a23c12e7ae490e59a1f18572ac35
Yash Jaiswal <jais.yash01@gmail.com>
2020-05-27 11:31:42 +0530

    backend request controller

--- app/controllers/requestController.js
+++ app/controllers/requestController.js
@@ -2,10 +2,25 @@ const Request = require('../models/request')
 const pick = require('lodash/pick')
 
 module.exports.list = (req, res) => {
-    Request.find()
-        .then(req => res.send(req))
-        .catch(err => res.send(err))
+    // if the user is petowner, he will be the reciever in the request model
+    const role = req.user.role
+    console.log(role)
+    if (role == "petsitter") {
+        Request.find({ receiver: req.user._id })
+            .then(req => res.send(req))
+            .catch(err => res.send(err))
+    } else if (role == "petowner") {
+        Request.find({ user: req.user._id })
+            .then(req => res.send(req))
+            .catch(err => res.send(err))
+    } else {
+        // this is the admin
+        Request.find()
+            .then(req => res.send(req))
+            .catch(err => res.send(err))
+    }
 }
+
 module.exports.create = (req, res) => {
     const body = pick(req.body, ['user', 'receiver', 'services', 'message', 'status'])
     const request = new Request(body)
@@ -13,16 +28,25 @@ module.exports.create = (req, res) => {
         .then(req => res.send(req))
         .catch(err => res.send(err))
 }
-module.exports.show = (req, res) => {
-    Request.find()
-        .then(req => res.send(req))
-        .catch(err => res.send(err))
-}
+
 module.exports.update = (req, res) => {
-    Request.find()
-        .then(req => res.send(req))
+    const role = req.user.role
+    const _id = req.params.id
+    Request.findOne({ _id })
+        .then(req => {
+            if (req.status == "true") {
+                res.send("can't be updated")
+            } else {
+                // check for the owner
+            }
+            // res.send(req)
+        })
         .catch(err => res.send(err))
+    if (role == "petsitter") {
+        // can only update 
+    }
 }
+
 module.exports.delete = (req, res) => {
     Request.find()
         .then(req => res.send(req))
--- config/routes.js
+++ config/routes.js
@@ -13,15 +13,15 @@ const authorizeUser = require('../app/middlewares/authorizeUser')
 router.get('/users', authenticateUser, authorizeUser.admin, usersController.users)
 router.post('/users/register', usersController.register)
 router.post('/users/login', usersController.login)
-router.get('/users/account', authenticateUser, usersController.account) 
+router.get('/users/account', authenticateUser, usersController.account)
 router.delete('/users/logout', authenticateUser, usersController.logout)
 
 router.get('/pets-all', authenticateUser, authorizeUser.admin, petsController.listAll)
-router.get('/pets',authenticateUser, authorizeUser.petOwner, petsController.list)
-router.post('/pets',authenticateUser, authorizeUser.petOwner, petsController.create)
-router.get('/pets/:id',authenticateUser, authorizeUser.petOwner, petsController.show)
-router.put('/pets/:id',authenticateUser, authorizeUser.petOwner, petsController.update)
-router.delete('/pets/:id',authenticateUser, authorizeUser.petOwner, petsController.destroy)
+router.get('/pets', authenticateUser, authorizeUser.petOwner, petsController.list)
+router.post('/pets', authenticateUser, authorizeUser.petOwner, petsController.create)
+router.get('/pets/:id', authenticateUser, authorizeUser.petOwner, petsController.show)
+router.put('/pets/:id', authenticateUser, authorizeUser.petOwner, petsController.update)
+router.delete('/pets/:id', authenticateUser, authorizeUser.petOwner, petsController.destroy)
 
 router.get('/profile-all', authenticateUser, profileController.listAll)
 router.get('/profile', authenticateUser, profileController.list)
@@ -35,10 +35,10 @@ router.get('/reviews/:id', authenticateUser, reviewController.show)
 router.put('/reviews/:id', authenticateUser, reviewController.update)
 router.delete('/reviews/:id', authenticateUser, reviewController.destroy)
 
-router.get('/request', authenticateUser, authorizeUser.petOwner, requestController.list)
-router.post('/request', authenticateUser, authorizeUser.petOwner, requestController.create)
-router.get('/request/:id', authenticateUser, authorizeUser.petOwner, requestController.show)
-router.get('/request/:id', authenticateUser, requestController.update) // anybody can update it, let's say petsitter say yes or petowner wants to change the message body
+router.get('/request', authenticateUser, requestController.list) // can be accesessed by everyone 
+router.post('/request', authenticateUser, authorizeUser.petOwner, requestController.create) // can be created only by petowner
+// router.get('/request/:id', authenticateUser, requestController.show) // can be accesessed by everyone/// there will be no provision of show of request
+router.put('/request/:id', authenticateUser, requestController.update) // anybody can update it, let's say petsitter say yes or petowner wants to change the message body
 router.get('/request/:id', authenticateUser, authorizeUser.petOwner, requestController.delete)
 
 router.get('/services-all', authenticateUser, authorizeUser.admin, serviceController.list)
